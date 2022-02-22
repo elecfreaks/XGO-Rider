e3ea64780347cd4f5b4f3c1c73e085b19f4ece50
elecfreaks <support@elecfreaks.com>
2022-02-22 16:40:39 +0800

    modify enum

--- main.ts
+++ main.ts
@@ -4,106 +4,106 @@ namespace xgo {
 
     export enum action_enum {
         //% block="Default_posture"
-        Default_posture = "\x00",
+        Default_posture,
         //% block="Go_prone"
-        Go_prone = "\x01",
+        Go_prone,
         //% block="Stand"
-        Stand = "\x02",
+        Stand,
         //% block="Crawl_forward"
-        Crawl_forward = "\x03",
+        Crawl_forward,
         //% block="Whirl"
-        Whirl = "\x04",
+        Whirl,
         //% block="Sur_place"
-        Sur_place = "\x05",
+        Sur_place,
         //% block="Squat"
-        Squat = "\x06",
+        Squat,
         //% block="Twirl_Roll"
-        Twirl_Roll = "\x07",
+        Twirl_Roll,
         //% block="Twirl_Pitch"
-        Twirl_Pitch = "\x08",
+        Twirl_Pitch,
         //% block="Twirl_Yaw"
-        Twirl_Yaw = "\x09",
+        Twirl_Yaw,
         //% block="Triaxial_rotation"
-        Triaxial_rotation = "\x0A",
+        Triaxial_rotation,
         //% block="Pee"
-        Pee = "\x0B",
+        Pee,
         //% block="Sit_down"
-        Sit_down = "\x0C",
+        Sit_down,
         //% block="Wave"
-        Wave = "\x0D",
+        Wave,
         //% block="Stretch_oneself"
-        Stretch_oneself = "\x0E",
+        Stretch_oneself,
         //% block="Play_pendulum"
-        Play_pendulum = "\x10",
+        Play_pendulum,
         //% block="Request_feeding"
-        Request_feeding = "\x11",
+        Request_feeding,
         //% block="Looking_for_food"
-        Looking_for_food = "\x12",
+        Looking_for_food,
         //% block="Handshake"
-        Handshake = "\x13"
+        Handshake
     }
 
     //% block="Execution action %action"
     export function execution_action(action: action_enum) {
         serial.writeString("\x55\x00\x09\x00")
-        serial.writeString("\x3E" + action)
+        serial.writeString("\x3E")
         switch (action) {
             case action_enum.Default_posture:
-                serial.writeString("\xB8\x00\xAA")
+                serial.writeString("\x00\xB8\x00\xAA")
                 break
             case action_enum.Go_prone:
-                serial.writeString("\xB7\x00\xAA")
+                serial.writeString("\x01\xB7\x00\xAA")
                 break
             case action_enum.Stand:
-                serial.writeString("\xB6\x00\xAA")
+                serial.writeString("\x02\xB6\x00\xAA")
                 break
             case action_enum.Crawl_forward:
-                serial.writeString("\xB5\x00\xAA")
+                serial.writeString("\x03\xB5\x00\xAA")
                 break
             case action_enum.Whirl:
-                serial.writeString("\xB4\x00\xAA")
+                serial.writeString("\x04\xB4\x00\xAA")
                 break
             case action_enum.Sur_place:
-                serial.writeString("\xB3\x00\xAA")
+                serial.writeString("\x05\xB3\x00\xAA")
                 break
             case action_enum.Squat:
-                serial.writeString("\xB2\x00\xAA")
+                serial.writeString("\x06\xB2\x00\xAA")
                 break
             case action_enum.Twirl_Roll:
-                serial.writeString("\xB1\x00\xAA")
+                serial.writeString("\x07\xB1\x00\xAA")
                 break
             case action_enum.Twirl_Pitch:
-                serial.writeString("\xB0\x00\xAA")
+                serial.writeString("\x08\xB0\x00\xAA")
                 break
             case action_enum.Twirl_Yaw:
-                serial.writeString("\xAF\x00\xAA")
+                serial.writeString("\x09\xAF\x00\xAA")
                 break
             case action_enum.Triaxial_rotation:
-                serial.writeString("\xAE\x00\xAA")
+                serial.writeString("\x0A\xAE\x00\xAA")
                 break
             case action_enum.Pee:
-                serial.writeString("\xAD\x00\xAA")
+                serial.writeString("\x0B\xAD\x00\xAA")
                 break
             case action_enum.Sit_down:
-                serial.writeString("\xAC\x00\xAA")
+                serial.writeString("\x0C\xAC\x00\xAA")
                 break
             case action_enum.Wave:
-                serial.writeString("\xAB\x00\xAA")
+                serial.writeString("\x0D\xAB\x00\xAA")
                 break
             case action_enum.Stretch_oneself:
-                serial.writeString("\xAA\x00\xAA")
+                serial.writeString("\x0E\xAA\x00\xAA")
                 break
             case action_enum.Play_pendulum:
-                serial.writeString("\xA8\x00\xAA")
+                serial.writeString("\x10\xA8\x00\xAA")
                 break
             case action_enum.Request_feeding:
-                serial.writeString("\xA7\x00\xAA")
+                serial.writeString("\x11\xA7\x00\xAA")
                 break
             case action_enum.Looking_for_food:
-                serial.writeString("\xA6\x00\xAA")
+                serial.writeString("\x12\xA6\x00\xAA")
                 break
             case action_enum.Handshake:
-                serial.writeString("\xA5\x00\xAA")
+                serial.writeString("\x13\xA5\x00\xAA")
                 break
         }
     }
