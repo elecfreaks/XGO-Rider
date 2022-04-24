661a1e28dc679fccc291040ec73bfafbf655fb55
elecfreaks <support@elecfreaks.com>
2022-04-24 17:00:00 +0800

    add init_action function

--- main.ts
+++ main.ts
@@ -973,4 +973,19 @@ namespace xgo {
         }
         serial.writeBuffer(commands_buffer)
     }
+
+    //% block="restore initial action"
+    export function init_action() {
+        let commands_buffer = pins.createBuffer(9)
+        commands_buffer[0] = 0x55
+        commands_buffer[1] = 0x00
+        commands_buffer[2] = 0x09
+        commands_buffer[3] = 0x00
+        commands_buffer[4] = 0x5D
+        commands_buffer[5] = 0x01
+        commands_buffer[6] = ~(0x09 + 0x00 + 0x5D + 0x01)
+        commands_buffer[7] = 0x00
+        commands_buffer[8] = 0xAA
+        serial.writeBuffer(commands_buffer)
+    }
 }
